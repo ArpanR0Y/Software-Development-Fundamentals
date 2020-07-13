fcaaa882a46ba5a690bac077df47bcf49c20fd07
Arpan Roy <royarpan09@gmail.com>
2020-07-13 08:51:26 +0530

    WhackAMole Test Cases added and out folder added to .gitignore

--- WhackAMole/src/main/java/WhackAMole.java
+++ WhackAMole/src/main/java/WhackAMole.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -12,32 +13,38 @@ public class WhackAMole {
   public WhackAMole(int numAttempts, int gridDimension) {
     this.attemptsLeft = numAttempts;
     this.moleGrid = new char[gridDimension][gridDimension];
-  }
-
-  private void place(int x, int y) {
-    if (moleGrid[x][y] == '\u0000') {
-      moleGrid[x][y] = 'M';
-      molesLeft--;
+    for (char[] r : moleGrid) {
+      Arrays.fill(r, '*');
     }
   }
 
-  private void whack(int x, int y) {
+  public boolean place(int x, int y) {
+    if (moleGrid[x][y] == 'M') {
+      return false;
+    }
+    moleGrid[x][y] = 'M';
+    molesLeft++;
+    return true;
+  }
+
+  public void whack(int x, int y) {
     if (moleGrid[x][y] == 'M') {
       moleGrid[x][y] = 'X';
       score++;
+      molesLeft--;
     } else {
       moleGrid[x][y] = 'W';
     }
     attemptsLeft--;
   }
 
-  private void printGridToUser() {
+  public void printGridToUser() {
     for (char[] row : moleGrid) {
       for (char element : row) {
-        if (element == 'X') {
-          System.out.print(" X ");
-        } else if (element == 'W') {
+        if (element == 'W') {
           System.out.print(" W ");
+        } else if (element == 'X') {
+          System.out.print(" X ");
         } else {
           System.out.print(" * ");
         }
@@ -46,18 +53,10 @@ public class WhackAMole {
     }
   }
 
-  private void printGrid() {
+  public void printGrid() {
     for (char[] row : moleGrid) {
       for (char element : row) {
-        if (element == 'M') {
-          System.out.print(" M ");
-        } else if (element == 'W') {
-          System.out.print(" W ");
-        } else if (element == 'X') {
-          System.out.print(" X ");
-        } else {
-          System.out.print(" * ");
-        }
+        System.out.print(" "+element+" ");
       }
       System.out.println();
     }
@@ -72,9 +71,7 @@ public class WhackAMole {
 
     WhackAMole whackAMole = new WhackAMole((gridSize * 2) - 1, gridSize);
 
-    whackAMole.molesLeft = gridSize;
-
-    while (whackAMole.molesLeft > 0) {
+    while (whackAMole.molesLeft < gridSize) {
       whackAMole.place(random.nextInt(gridSize), random.nextInt(gridSize));
     }
 
---
+++ WhackAMole/src/test/java/WhackAMoleTest.java
@@ -0,0 +1,121 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WhackAMoleTest {
+
+  private WhackAMole whack;
+
+  @Before
+  public void setUp() {
+    whack = new WhackAMole(5, 3);
+  }
+
+  @Test
+  public void testConstructorNumOfAttempts() {
+    int expected = 5;
+    int actual = whack.attemptsLeft;
+    assertEquals("Expected number of attempts: " + expected + ", but actual: " + actual, expected,
+        actual);
+  }
+
+  @Test
+  public void testConstructorGrid() {
+    int expectedRow = 3;
+    int actualRow = whack.moleGrid.length;
+    assertEquals("Expected number of rows: " + expectedRow + ", but actual: " + actualRow,
+        expectedRow, actualRow);
+    for (int i = 0; i < 3; i++) {
+      int expectedCol = 3;
+      int actualCol = whack.moleGrid[i].length;
+      assertEquals("Expected number of columns for row " + i + ": " + expectedCol + ", but "
+          + "actual: " + actualCol, expectedCol, actualCol);
+      for (int j = 0; j < 3; j++) {
+        char expectedChar = '*';
+        char actualChar = whack.moleGrid[i][j];
+        assertEquals(
+            "Expected char at row " + i + " column " + j + " is " + expectedChar + ", but "
+                + "actual " + actualChar, expectedChar, actualChar);
+      }
+    }
+  }
+
+  @Test
+  public void testPlaceSucceed() {
+    assertTrue(whack.place(1, 1));
+    assertTrue(whack.place(1, 2));
+    int expectedNumMoles = 2;
+    int actualNumMoles = whack.molesLeft;
+    assertEquals("Expected number of moles: " + expectedNumMoles + ", but "
+        + "actual: " + actualNumMoles, expectedNumMoles, actualNumMoles);
+    char expectedOneOne = 'M';
+    char actualOneOne = whack.moleGrid[1][1];
+    assertEquals("Expected char at (1, 1): M, but actual: " + actualOneOne, expectedOneOne,
+        actualOneOne);
+    assertEquals("Expected char at (1, 2): M, but actual: " + whack.moleGrid[1][2], 'M',
+        whack.moleGrid[1][2]);
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (!((i == 1 && j == 1) || (i == 1 && j == 2))) {
+          assertEquals("Mole placed at wrong place: (" + i + ", " + j + ")", '*',
+              whack.moleGrid[i][j]);
+        }
+      }
+    }
+  }
+
+  @Test
+  public void testPlaceFail() {
+    assertTrue(whack.place(1, 1));
+    assertFalse(whack.place(1, 1));
+    assertEquals("(1, 1) doesn't have a mole", 'M', whack.moleGrid[1][1]);
+  }
+
+  @Test
+  public void testWhackSucceedAttempts() {
+    whack.place(1, 1);
+    assertEquals(
+        "Expected number of attemptsLeft before whack: 5, but actual: " + whack.attemptsLeft, 5,
+        whack.attemptsLeft);
+    whack.whack(1, 1);
+    assertEquals("Expected number of attempts after whack: 4, but actual: " + whack.attemptsLeft,
+        4, whack.attemptsLeft);
+  }
+
+  @Test
+  public void testWhackFailAttempts() {
+    whack.place(1, 1);
+    assertEquals(
+        "Expected number of attemptsLeft before whack: 5, but actual: " + whack.attemptsLeft, 5,
+        whack.attemptsLeft);
+    whack.whack(0, 0);
+    assertEquals("Expected number of attempts after whack: 4, but actual: " + whack.attemptsLeft,
+        4, whack.attemptsLeft);
+  }
+
+  @Test
+  public void testWhackSucceedMolesLeft() {
+    whack.place(1, 1);
+    assertEquals("Expected number of moles left before whack: 1, but actual: " + whack.molesLeft,
+        1, whack.molesLeft);
+    whack.whack(1, 1);
+    assertEquals(
+        "Expected number of moles left after successful whack: 0, but actual: " + whack.molesLeft,
+        0, whack.molesLeft);
+  }
+
+  @Test
+  public void testWhackFailMolesLeft() {
+    whack.place(1, 1);
+    assertEquals("Expected number of moles left before whack: 1, but actual: " + whack.molesLeft,
+        1, whack.molesLeft);
+    whack.whack(0, 0);
+    assertEquals(
+        "Expected number of moles left after failed whack: 1, but actual: " + whack.molesLeft, 1,
+        whack.molesLeft);
+  }
+
+}
