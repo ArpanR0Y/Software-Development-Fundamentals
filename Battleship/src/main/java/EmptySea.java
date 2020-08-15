5dd5b00afc6218846470acbe26636cfd8f2df603
Arpan Roy <royarpan09@gmail.com>
2020-08-15 19:04:49 +0530

    Project-Battleship: Constructor added to the Ocean class and Java Docs added to EmptySea and Ocean class

--- Battleship/src/main/java/EmptySea.java
+++ Battleship/src/main/java/EmptySea.java
@@ -1,26 +1,43 @@
 public class EmptySea extends Ship {
 
+  /**
+   * This constructor sets the inherited length variable to 1.
+   */
   public EmptySea() {
     this.setLength(1);
     this.setHit();
   }
 
+  /**
+   * This method overrides shootAt(int row, int column) that is inherited from Ship, and always
+   * returns false to indicate that nothing was hit.
+   */
   @Override
   public boolean shootAt(int row, int column) {
     this.getHit()[0] = true;
     return false;
   }
 
+  /**
+   * This method overrides isSunk() that is inherited from Ship, and always returns false to
+   * indicate that you didn’t sink anything.
+   */
   @Override
   public boolean isSunk() {
     return false;
   }
 
+  /**
+   * Returns a single-character String to use in the Ocean’s print method.
+   */
   @Override
   public String toString() {
     return this.getHit()[0] ? "-" : ".";
   }
 
+  /**
+   * This method just returns the string ”empty”
+   */
   @Override
   public String getShipType() {
     return "empty";
